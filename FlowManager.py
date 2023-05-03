54faa96045d2d4353f36bcf72e95636237a19a1e
MichalMarom <100923568+MichalMarom@users.noreply.github.com>
2023-05-03 19:11:17 +0300

    call tabu

--- FlowManager.py
+++ FlowManager.py
@@ -34,6 +34,6 @@ class FlowManager:
 
     def solve_CVRP(self):
         self.population.create_clusters()
-        self.population.solve_clustrers_TSP(ACO)
+        self.population.solve_clustrers_TSP(Tabu_search)
 
         return
